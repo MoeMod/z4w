e2639486d094ba170d2c25d39646e39707e1c5c9
hymei <hymei@tencent.com>
2021-03-11 18:48:47 +0800

    timer with engine clock

--- z4w_amxx/amxx/amxmodx_detail.cpp
+++ z4w_amxx/amxx/amxmodx_detail.cpp
@@ -13,8 +13,9 @@ namespace amxx
 {
 	namespace detail {
 		boost::asio::io_context ioc;
-		using timer_type = boost::asio::high_resolution_timer;
-		std::multimap<int, std::weak_ptr<boost::asio::high_resolution_timer>> timer_map;
+		// 这里改用引擎虚拟时钟，避免和操作系统耦合导致系统时间变更的时候出现异常行为
+		using timer_type = boost::asio::basic_waitable_timer<EngineClock>;
+		std::multimap<int, std::weak_ptr<timer_type>> timer_map;
 
 		void Timer_StartFrame()
 		{
@@ -30,7 +31,7 @@ namespace amxx
 		{
 			using namespace detail;
 			std::shared_ptr<timer_type> timer = std::make_shared<timer_type>(ioc);
-			timer->expires_from_now(std::chrono::duration_cast<timer_type::duration>(time));
+			timer->expires_from_now(time);
 			timer_map.emplace(0, timer);
 			timer->async_wait([f = std::move(function), timer](const std::error_code& ec) {
 				if (!ec)
